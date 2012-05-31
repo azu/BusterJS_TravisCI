a3dc7891992c8f099c6cd3ac2d7f2084bc25ec45
azu <azuciao@gmail.com>
2012-05-31 23:31:31 +0900

    assert

--- test/hello-test.js
+++ test/hello-test.js
@@ -3,6 +3,7 @@
  * Date: 12/05/31 22:46
  * License: MIT License
  */
+var assert = buster.assertions.assert;
 buster.testCase("Src Test", {
     "should be HELLO" : function(){
         assert.equals(hello(), "HELLO");
