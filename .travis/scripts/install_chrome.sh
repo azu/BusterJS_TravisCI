848f57b3f6351762dde58f39e6a230655978ce54
azu <azuciao@gmail.com>
2013-02-26 22:59:06 +0900

    google chrome with no-sandbox

--- .travis/scripts/install_chrome.sh
+++ .travis/scripts/install_chrome.sh
@@ -6,3 +6,4 @@ BASEDIR=$(readlink -f "$BASEDIR/..")
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update --force-yes
 sudo apt-get install -qq --force-yes xvfb imagemagick google-chrome-stable
+sudo apt-get install gconf2 # for firefox
