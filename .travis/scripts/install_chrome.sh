50b24749efde1cdf5469f0ec8ac95d55930c4336
azu <azuciao@gmail.com>
2013-03-05 00:50:42 +0900

    [from now] 2013/03/05 00:50:42
    diff --git a/.travis/scripts/install_chrome.sh b/.travis/scripts/install_chrome.sh
    index 974ee54..b1531a0 100755
    --- a/.travis/scripts/install_chrome.sh
    +++ b/.travis/scripts/install_chrome.sh
    @@ -5,6 +5,6 @@ BASEDIR=$(readlink -f "$BASEDIR/..")
    
     sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
     sudo apt-get update --force-yes
    -sudo apt-get install -qq --force-yes xvfb imagemagick google-chrome-stable
    -sudo apt-get install gconf2 # for firefox
    -sudo chcon -t usr_t /opt/google/chrome/chrome-sandbox
    \ No newline at end of file
    +sudo apt-get install -qq --force-yes imagemagick google-chrome-stable
    +# sudo apt-get install gconf2 # for firefox
    +# sudo chcon -t usr_t /opt/google/chrome/chrome-sandbox
    \ No newline at end of file

--- .travis/scripts/install_chrome.sh
+++ .travis/scripts/install_chrome.sh
@@ -5,6 +5,6 @@ BASEDIR=$(readlink -f "$BASEDIR/..")
 
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update --force-yes
-sudo apt-get install -qq --force-yes xvfb imagemagick google-chrome-stable
-sudo apt-get install gconf2 # for firefox
-sudo chcon -t usr_t /opt/google/chrome/chrome-sandbox
+sudo apt-get install -qq --force-yes imagemagick google-chrome-stable
+# sudo apt-get install gconf2 # for firefox
+# sudo chcon -t usr_t /opt/google/chrome/chrome-sandbox
