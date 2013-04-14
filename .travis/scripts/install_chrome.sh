fc7d97779f615045ec5c9bb0fad2bc4dd7aa3ed9
azu <azuciao@gmail.com>
2013-04-14 10:07:43 +0900

    remove fix permission

--- .travis/scripts/install_chrome.sh
+++ .travis/scripts/install_chrome.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# Make sure /dev/shm has correct permissions.
-ls -l /dev/shm
-sudo chmod 1777 /dev/shm
-ls -l /dev/shm
-
 echo "Getting $VERSION of　Chrome-stable"
 export CHROME=google-chrome-stable_current_amd64.deb
 wget https://dl.google.com/linux/direct/$CHROME
