84d96c19ffc9a0ec9cc170e34e0ad9338686b34f
azu <azuciao@gmail.com>
2013-04-14 10:03:30 +0900

    use stable chrome

--- .travis/scripts/install_chrome.sh
+++ .travis/scripts/install_chrome.sh
@@ -5,8 +5,8 @@ ls -l /dev/shm
 sudo chmod 1777 /dev/shm
 ls -l /dev/shm
 
-echo "Getting $VERSION of　Chrome-beta"
-export CHROME=google-chrome-beta_current_amd64.deb
+echo "Getting $VERSION of　Chrome-stable"
+export CHROME=google-chrome-stable_current_amd64.deb
 wget https://dl.google.com/linux/direct/$CHROME
 sudo dpkg --install $CHROME || sudo apt-get -f install
 ls -l /usr/bin/google-chrome
